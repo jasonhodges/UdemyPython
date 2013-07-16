7134ffc0432593d448634e3e2748f993bee144e0
Jason Hodges <jason@jasonhodges.me>
2013-07-16 17:00:21 -0400

    Lecture 18: intro to strings in Python, H5v1

---
+++ HLevel5/H5video1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#strings
+print ('this is a string')
+
+s = 'this is a string'
+print s.upper()
+
+print ('this is a string'.upper())
+
+print ('THIS IS A STRING'.lower())
+
+print ('This Is A String'.swapcase())
+
+print ('THIS IS A STRING'.find('IS'))
+
+print('this is a string'.replace('this', 'that'))
+
+
