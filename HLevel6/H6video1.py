16f95303324f7fee535e4769e6c7e63c6dfaa8c0
Jason Hodges <jason@jasonhodges.me>
2013-07-16 17:02:01 -0400

    Lecture 21: about arrays in Python, H6v1

---
+++ HLevel6/H6video1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#About Arrays in Python
+
+t = (1, 2, 3, 4, 5, 6)
+print t
+
+print type(t)
+
+print len(t)n\
+
+print min(t)
+
+print max(t)
+
+print t[3]
+
+for i in t: print (i)
+
