84d8921df107d3c9ea1c46d46a80abf67877b8e2
Jason Hodges <jason@jasonhodges.me>
2013-07-19 09:07:17 -0400

    Lecture 27: More about functions in Python

---
+++ HLevel7/H7video2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#More about Functions in Python
+
+def main():
+    for i in igneus(): print(i)
+    
+def igneus():
+    return range(30)
+    
+        
+if __name__ == "__main__": main()
