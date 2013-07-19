34cbf82f4879e57893e843fea66ab303a7292283
Jason Hodges <jason@jasonhodges.me>
2013-07-19 09:08:01 -0400

    Lecture 28: Examples for creating our own functions in Python, H7v3

---
+++ HLevel7/H7video3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+#Examples for creating our own functions in Python
+
+def main():
+    for i in irange(0, 30, 1): print(i, end = ' ')
+    
+def irange(beg, end, jump):
+    
+        
+if __name__ == "__main__": main()
